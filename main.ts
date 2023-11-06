ce7836035c9192e70f17d2f7485103cac100eeb9
pencilrocketman <sfcmurata@gmail.com>
2023-11-05 20:51:33 -0800

    first commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+import blog from "https://deno.land/x/blog@0.7.0/blog.tsx";
+
+blog({
+  author: "Masayuki Murata",
+  title: "HoopL",
+  description: "Blog focus on Tech, Learing English, Basketball.",
+  avatar: "https://deno-avatar.deno.dev/avatar/20e51a.svg",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "GitHub", url: "https://github.com/pencilrocketman" },
+    { title: "Twitter", url: "https://twitter.com/pencilrocketman" },
+  ],
+  lang: "en",
+  favicon: "https://deno-avatar.deno.dev/avatar/20e51a.svg"
+});
---
+++ posts/hello_world.md
@@ -0,0 +1,13 @@
+---
+title: Hello world!
+publish_date: 2023-11-06
+---
+
+This is my first blog post in deno land!
+
+This blog is written by [deno_blog](https://github.com/denoland/deno_blog).
+
+Thank you developers related to deno_blog!
+
+This software is easy to use and I understood deno community's effort.
+
